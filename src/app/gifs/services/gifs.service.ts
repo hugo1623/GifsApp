3fddf6a2d04d0342eb607998c15deed5563574c4
Hugo Luis <hugoluis.system@gmail.com>
2023-04-03 01:20:12 -0500

    Peticiones HTTP

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,21 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
+
+  private apiKey: string= 'ESPNNn9nAdQMqpGrk131kdxMw8HI7WFo';
   private _historial: string[] = [];
 
   get historial() {
     return [...this._historial];
   }
 
+  constructor(private http: HttpClient){}
+
   buscarGifs(query: string) {
+
     query = query.trim().toLocaleUpperCase();
+
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
     }
-    this._historial = this._historial.splice(0, 10);
-    console.log(this._historial);
+    this.http.get('https://api.giphy.com/v1/gifs/search?apikey=ESPNNn9nAdQMqpGrk131kdxMw8HI7WFo&q=dbz')
+      .subscribe((resp: any) =>{
+        console.log(resp.data);
+      });
   }
 }
