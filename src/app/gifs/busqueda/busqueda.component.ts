dca383e7660203020a1da9be1299c2da301c6860
Hugo Luis <hugoluis.system@gmail.com>
2023-04-02 23:04:04 -0500

    Creación del Servicio Gifs

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,17 +1,18 @@
-import { Component, ElementRef, ViewChild} from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
-  styleUrls: ['./busqueda.component.css']
+  styleUrls: ['./busqueda.component.css'],
 })
-export class BusquedaComponent{
- 
-  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
-  buscar(){
+export class BusquedaComponent {
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
-  const valor = this.txtBuscar.nativeElement.value;
-  console.log(valor);
-  this.txtBuscar.nativeElement.value = '';
+  constructor(private gifsService: GifsService) {}
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+    this.gifsService.buscarGifs(valor);
+    this.txtBuscar.nativeElement.value = '';
   }
 }
