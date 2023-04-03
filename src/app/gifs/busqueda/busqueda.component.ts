0bcdf3939a9f2dfd02cef4ca0c2b3b634740c099
Hugo Luis <hugoluis.system@gmail.com>
2023-04-02 22:26:01 -0500

    Utilizando el Decorador @ViewChild en el Componente Buscar

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
   styleUrls: ['./busqueda.component.css']
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent{
+ 
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+  buscar(){
 
-  constructor() { }
-
-  ngOnInit(): void {
+  const valor = this.txtBuscar.nativeElement.value;
+  console.log(valor);
+  this.txtBuscar.nativeElement.value = '';
   }
-
 }
